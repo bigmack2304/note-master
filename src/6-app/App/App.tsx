460c27f30a007ed82c79b595c39c25de45e54f8f
bigmack2304 <dumon7@yandex.ru>
2024-01-24 16:11:40 +0700

    init commit

--- src/6-app/App/App.tsx
+++ src/6-app/App/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import {logo} from '0-shared';
 import './App.css';
 
 function App() {
+  const i = process.env.REACT_APP_TEST_VARIABLE ?? "null";
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <span>process env: TEST: {i}</span>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/6-app/index.ts
@@ -0,0 +1,5 @@
+import App from "./App/App"
+import normalize from "./normalize/normalize.module.scss"
+import baseStyles from "./baseStyles/baseStyles.module.scss"
+
+export {App, normalize, baseStyles}
