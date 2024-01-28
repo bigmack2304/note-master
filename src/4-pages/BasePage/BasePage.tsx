324ab8eb404f0b7cfb5303a09194cbb4e0d59f39
bigmack2304 <dumon7@yandex.ru>
2024-01-29 00:07:10 +0700

    dev

--- src/4-pages/BasePage/BasePage.tsx
+++ src/4-pages/BasePage/BasePage.tsx
@@ -3,6 +3,7 @@ import "./BasePage.scss";
 import { AppBar } from "0-shared";
 import { ToggleMenuButton } from "2-features";
 import { HeaderBar } from "3-widgets";
+import { WorkSpace } from "0-shared";
 
 type Tprops = {
     addClassNames?: string[];
@@ -15,6 +16,7 @@ function BasePage({ addClassNames = [] }: Tprops) {
     return (
         <div className={genClassName}>
             <HeaderBar></HeaderBar>
+            <WorkSpace></WorkSpace>
         </div>
     );
 }
