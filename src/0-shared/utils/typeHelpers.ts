97a468dbef481f0cd904c7c4798a141314586d0e
bigmack2304 <dumon7@yandex.ru>
2024-02-01 00:44:46 +0700

    новый хелпер для определения типа

--- src/0-shared/utils/typeHelpers.ts
+++ src/0-shared/utils/typeHelpers.ts
@@ -1,4 +1,10 @@
+import { ForwardRefRenderFunction } from "react";
+
 // вычисляет пропсы компонента
 type GetProps<T> = T extends React.ComponentType<infer Props> ? Props : unknown;
 
-export type { GetProps };
+// типизирует параметр ref в компоненте, если используется forwardRef
+// в качестве T применяется тип элемента на который перенаправляется ref (например HTMLInputElement)
+type Ref<T> = Parameters<ForwardRefRenderFunction<T>>[1];
+
+export type { GetProps, Ref };
