1be64d82b22345011f37bd68ceb2c2206b6b8411
bigmack2304 <dumon7@yandex.ru>
2024-02-01 20:48:19 +0700

    обновил функционал useHandleUpdate
    - теперь вызов возвращаемой функции может выполнятся с задержкой

--- src/0-shared/hooks/useHandleUpdate.ts
+++ src/0-shared/hooks/useHandleUpdate.ts
@@ -1,16 +1,30 @@
 import React, { useState, useCallback } from "react";
+import { firstCallerDelayCallback } from "0-shared/utils/decorators";
 
 // хук на случай если понадобится ручное обновление компонента
 // handleUpdate функция неизменна между рендерами
+//
+// если вызвать хук с параметром true то возвращенный [handleUpdate] будет обернут в декоратор
+// для предотвращения вызовов с большой частотой
 
-function useHandleUpdate(): [handleupdate: () => void] {
-    let [HandleUpdateVal, setHandleUpdateVal] = useState<boolean>(true);
+function useHandleUpdate(isUseLowUpdate?: boolean): [handleUpdate: () => void] {
+  let [HandleUpdateVal, setHandleUpdateVal] = useState<boolean>(true);
 
-    const handleUpdate = useCallback(() => {
+  const handleUpdate = useCallback(() => {
+    setHandleUpdateVal((state) => !state);
+  }, []);
+
+  const handleUpdateLowUpdate = useCallback(
+    firstCallerDelayCallback({
+      func: () => {
         setHandleUpdateVal((state) => !state);
-    }, []);
+      },
+    }),
+    []
+  );
 
-    return [handleUpdate];
+  if (isUseLowUpdate) return [handleUpdateLowUpdate];
+  return [handleUpdate];
 }
 
 export { useHandleUpdate };
