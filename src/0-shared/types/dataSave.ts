9693d555514e14b086d8e6183b9649a3eba05f3b
bigmack2304 <dumon7@yandex.ru>
2024-02-03 00:09:22 +0700

    типизация для сохранаяемого\загружаемого фаила

---
+++ src/0-shared/types/dataSave.ts
@@ -0,0 +1,58 @@
+// варианты своиств в зависимости от компонента внутри заметки
+// заголовок
+interface TBodyComponentHeader {
+    component: "header";
+    value: string;
+}
+
+// текст
+interface TBodyComponentText {
+    component: "text";
+    texValue: string;
+}
+//////////////////////////////////////////////////////////////////
+
+// общие поля компонентов внутри заметки
+interface IDataTreeitemBody {
+    note_сomponent_id: string; // локальный id компонента внутри заметки
+}
+
+// тип поля body в заметке
+// TODO: потом тип пойдет по такому примеру
+//type TNoteBody = IDataTreeitemBody & (TBodyComponentHeader | TBodyComponentText);
+type TNoteBody = IDataTreeitemBody & TBodyComponentHeader;
+
+// типизация тега заметок
+interface IGlobalTag {
+    tag_name: string;
+    color: string;
+    text: string;
+}
+
+// тип, папка или заметка
+type TchildrenType = IDataTreeFolder | IDataTreeNote;
+
+// типизация заметки
+interface IDataTreeNote {
+    id: string; // глобальный id во всем дереве
+    tags?: IGlobalTag["tag_name"][];
+    name: string;
+    body: TNoteBody[];
+}
+
+// типизация папки
+interface IDataTreeFolder {
+    name: string;
+    color: string;
+    id: string; // глобальный id во всем дереве
+    children?: TchildrenType[];
+}
+
+// типизация фаила с сохранением
+interface IDataSave {
+    db_type: string;
+    global_tags: IGlobalTag[];
+    data_tree: IDataTreeFolder;
+}
+
+export type { IDataSave, IDataTreeFolder, IDataTreeNote, IGlobalTag, TNoteBody };
