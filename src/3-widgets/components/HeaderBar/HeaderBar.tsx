37edc28f5f431b14e182972fd3f18f51f0652e16
bigmack2304 <dumon7@yandex.ru>
2024-02-03 00:10:00 +0700

    теперь работает загрузка фаила

--- src/3-widgets/components/HeaderBar/HeaderBar.tsx
+++ src/3-widgets/components/HeaderBar/HeaderBar.tsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { AppBar } from "0-shared/components/AppBar/AppBar";
 import { ToggleMenuButton, ToggleMenuButtonMemo } from "2-features/components/ToggleMenuButton/ToggleMenuButton";
 import { SettingsContent } from "1-entities/components/SettingsContent/SettingsContent";
 import { DialogWindow } from "1-entities/components/DialogWindow/DialogWindow";
+import { InputFile } from "0-shared/components/InputFile/InputFile";
 
 type THeaderBarProps = {};
 
 function HeaderBar({}: THeaderBarProps) {
     const [isSettingsOpen, setIsSettingsOpen] = useState(false);
     const [isInfoOpen, setIsInfoOpen] = useState(false);
+    const inputFileRef = useRef<HTMLInputElement>(null);
 
     const onSettingsOpen = () => {
         setIsSettingsOpen(true);
@@ -26,13 +28,20 @@ function HeaderBar({}: THeaderBarProps) {
         setIsInfoOpen(false);
     };
 
+    const onLoadClick = (e: React.MouseEvent<Element, MouseEvent>) => {
+        if (!inputFileRef.current) return;
+
+        inputFileRef.current.click();
+    };
+
     return (
         <AppBar>
-            <ToggleMenuButtonMemo menuContentProps={{ onSettingsClick: onSettingsOpen, onInfoClick: onInfoOpen }} />
+            <ToggleMenuButtonMemo menuContentProps={{ onSettingsClick: onSettingsOpen, onInfoClick: onInfoOpen, onLoadClick: onLoadClick }} />
             <DialogWindow headerText="Меню настроек" isOpen={isSettingsOpen} onClose={onSettingsClose}>
                 <SettingsContent />
             </DialogWindow>
             <DialogWindow headerText="О приложении" isOpen={isInfoOpen} onClose={onInfoClose}></DialogWindow>
+            <InputFile ref={inputFileRef}></InputFile>
         </AppBar>
     );
 }
