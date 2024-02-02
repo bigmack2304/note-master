97c4b3572fe816901b47b39cee8efe670f597a31
bigmack2304 <dumon7@yandex.ru>
2024-02-03 00:00:47 +0700

    правки компонента загрузки фаила сохранения
    - в отдельный фаил вынес логику обработки сохранения

--- src/0-shared/components/InputFile/InputFile.tsx
+++ src/0-shared/components/InputFile/InputFile.tsx
@@ -1,12 +1,32 @@
-import React, { ForwardRefRenderFunction } from "react";
-import type { Ref, GetProps } from "0-shared/utils/typeHelpers";
+import React, { useRef, useImperativeHandle } from "react";
+import { useFileReader } from "0-shared/hooks/useFileReader";
+import type { Ref } from "0-shared/utils/typeHelpers";
+
+import { inputLoadStringHandler } from "2-features/utils/inputLoadStringHandler";
 
 type TInputFileProps = {
     inputSettings?: React.InputHTMLAttributes<HTMLInputElement>;
 };
 
-function InputFileComponent({ inputSettings }: TInputFileProps, ref: Ref<HTMLInputElement>) {
-    return <input className="visually_hidden" type="file" accept=".txt,.json" name="file" tabIndex={-1} ref={ref} {...inputSettings}></input>;
+function InputFileComponent({ inputSettings }: TInputFileProps, ref: Ref<HTMLInputElement | null>) {
+    const inputElement = useRef<HTMLInputElement>(null);
+
+    const onSucc = (val: any) => {
+        console.log(val);
+    };
+
+    const fileReader = useFileReader({ loadHandler: inputLoadStringHandler, loadSucessCallback: onSucc });
+
+    useImperativeHandle(ref, () => inputElement.current!, [inputElement.current]);
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file: File | null = e.target.files ? e.target.files[0] : null;
+
+        if (!file) return;
+        fileReader.readAsText(file);
+    };
+
+    return <input className="visually_hidden" type="file" accept=".txt,.json" name="file" tabIndex={-1} ref={inputElement} onChange={onInputChange} {...inputSettings}></input>;
 }
 
 const InputFile = React.forwardRef(InputFileComponent);
---
+++ src/2-features/utils/inputLoadStringHandler.ts
@@ -0,0 +1,13 @@
+type TAnyFunc = (...args: any[]) => void;
+
+function inputLoadStringHandler<returnType>(rawData: any) {
+    if (typeof rawData !== "string") return new Error("format not supported");
+    const data = JSON.parse(rawData);
+
+    if (!("db_type" in data)) return new Error("the file is not a note_Master save");
+    if (data.db_type !== "note_Master") return new Error("the file is incompatible with this version of note_master");
+
+    return data as returnType;
+}
+
+export { inputLoadStringHandler };
