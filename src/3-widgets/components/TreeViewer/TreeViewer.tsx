1f2809ea301884ba583936524f8080c832771cbe
bigmack2304 <dumon7@yandex.ru>
2024-02-03 22:54:15 +0700

    первоначальные наработки по отрисовке  фаиловой структуры

--- src/3-widgets/components/TreeViewer/TreeViewer.tsx
+++ src/3-widgets/components/TreeViewer/TreeViewer.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { TreeView } from "@mui/x-tree-view/TreeView";
 import { TreeItem } from "@mui/x-tree-view/TreeItem";
 import Box from "@mui/material/Box";
 import type { SxProps } from "@mui/material";
+import { useIndexedDBTempDataUpdate } from "0-shared/hooks/useIndexedDBTempUpdate";
+import { getTempDataDB } from "2-features/utils/appIndexedDB";
+import type { IDataSave, IDataTreeFolder, TchildrenType, IDataTreeNote, nodeType } from "0-shared/types/dataSave";
+import { useAppDispatch } from "0-shared/hooks/useAppDispatch";
+import { setCurrentNote, setCurrentFolder } from "5-app/GlobalState/saveDataInspectStore";
 
 type TTreeViewerProps = {};
 
@@ -19,22 +24,61 @@ const treeViewerStyle: SxProps = {
 };
 
 function TreeViewer({}: TTreeViewerProps) {
+    const [dataValue, setDataValue] = useState<IDataSave>();
+    const [isNeedUpdate, setIsNeedUpdate] = useState<boolean>(false);
+    const dispatch = useAppDispatch();
+
+    useIndexedDBTempDataUpdate(() => {
+        setIsNeedUpdate(true);
+    });
+
+    if (isNeedUpdate) {
+        getTempDataDB({
+            callback: (val) => {
+                setDataValue(val);
+                setIsNeedUpdate(false);
+                console.dir(val);
+            },
+        });
+    }
+
+    const renderTree = (nodes: TchildrenType | undefined) => {
+        if (!nodes) return <></>;
+
+        type obj = {
+            [x: string]: any;
+        };
+        let nodeData: IDataTreeNote | Omit<IDataTreeFolder, "children"> | obj = {};
+
+        (function () {
+            for (let key in nodes) {
+                if (key === "children") continue;
+
+                nodeData[key] = (nodes as any)[key];
+            }
+        })();
+
+        const onClick = () => {
+            console.dir(nodeData);
+            if ((nodeData.type as nodeType) === "folder") {
+                dispatch(setCurrentFolder(nodeData as Omit<IDataTreeFolder, "children">));
+            }
+            if ((nodeData.type as nodeType) === "note") {
+                dispatch(setCurrentNote(nodeData as IDataTreeNote));
+            }
+        };
+
+        return (
+            <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name} onClick={onClick}>
+                {"children" in nodes && Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
+            </TreeItem>
+        );
+    };
+
     return (
         <Box sx={treeViewerStyle}>
-            <TreeView aria-label="multi-select" defaultCollapseIcon={<ExpandMoreIcon />} defaultExpandIcon={<ChevronRightIcon />} multiSelect>
-                <TreeItem nodeId="1" label="Applications">
-                    <TreeItem nodeId="2" label="Calendar" />
-                    <TreeItem nodeId="3" label="Chrome" />
-                    <TreeItem nodeId="4" label="Webstorm" />
-                </TreeItem>
-                <TreeItem nodeId="5" label="Documents">
-                    <TreeItem nodeId="6" label="MUI">
-                        <TreeItem nodeId="7" label="src">
-                            <TreeItem nodeId="8" label="index.js" />
-                            <TreeItem nodeId="9" label="tree-view.js" />
-                        </TreeItem>
-                    </TreeItem>
-                </TreeItem>
+            <TreeView aria-label="rich object" defaultCollapseIcon={<ExpandMoreIcon />} defaultExpanded={["root"]} defaultExpandIcon={<ChevronRightIcon />}>
+                {renderTree(dataValue?.data_tree)}
             </TreeView>
         </Box>
     );
