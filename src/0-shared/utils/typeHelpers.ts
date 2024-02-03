4f825975ad343fb7f290d6f23ac646d9224b07a2
bigmack2304 <dumon7@yandex.ru>
2024-02-03 22:43:20 +0700

    новые определители типов

--- src/0-shared/utils/typeHelpers.ts
+++ src/0-shared/utils/typeHelpers.ts
@@ -1,4 +1,5 @@
 import { ForwardRefRenderFunction } from "react";
+import type { IDataTreeNote, IDataTreeFolder, TNoteBody } from "0-shared/types/dataSave";
 
 // вычисляет пропсы компонента
 type GetProps<T> = T extends React.ComponentType<infer Props> ? Props : unknown;
@@ -7,4 +8,20 @@ type GetProps<T> = T extends React.ComponentType<infer Props> ? Props : unknown;
 // в качестве T применяется тип элемента на который перенаправляется ref (например HTMLInputElement)
 type Ref<T> = Parameters<ForwardRefRenderFunction<T>>[1];
 
+// проверяет чтобы сущьность пренадлежала к типу IDataTreeNote
+function isDataTreeNote(val: any): val is IDataTreeNote {
+    return val.type === "note";
+}
+
+// проверяет чтобы сущьность пренадлежала к типу IDataTreeFolder
+function isDataTreeFolder(val: any): val is IDataTreeFolder {
+    return val.type === "folder";
+}
+
+// проверяет чтобы сущьность пренадлежала к типу TNoteBody
+function isDataNoteBody(val: any): val is TNoteBody {
+    return val.type === "component";
+}
+
+export { isDataTreeNote, isDataTreeFolder, isDataNoteBody };
 export type { GetProps, Ref };
