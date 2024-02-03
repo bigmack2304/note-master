47a0aa18d26fc460ce0a01f0cf8a3947b275e1d0
bigmack2304 <dumon7@yandex.ru>
2024-02-03 22:47:27 +0700

    добавил тулбар

---
+++ src/3-widgets/components/ToolBar/ToolBar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { useAppSelector } from "0-shared/hooks/useAppSelector";
+import Collapse from "@mui/material/Collapse";
+import type { RootState } from "5-app/GlobalState/store";
+import type { SxProps } from "@mui/material";
+
+type TToolBarprops = {
+    addClassNames?: string[];
+};
+
+const toolbarStyle: SxProps = {
+    height: "30px",
+    backgroundColor: "red",
+};
+
+function ToolBar({ addClassNames = [] }: TToolBarprops) {
+    const defaultClassName = "ToolBar";
+    const genClassName = defaultClassName.split(" ").concat(addClassNames).join(" ");
+    const isActive = useAppSelector((state) => state.toolBar.isActive);
+
+    return (
+        <Collapse in={isActive} className={"ToolBar_wrapper"} orientation="vertical">
+            <Box className={genClassName} sx={toolbarStyle}></Box>
+        </Collapse>
+    );
+}
+
+export { ToolBar };
--- src/4-pages/BasePage/BasePage.tsx
+++ src/4-pages/BasePage/BasePage.tsx
@@ -4,6 +4,7 @@ import { HeaderBar } from "3-widgets/components/HeaderBar/HeaderBar";
 import { WorkSpace } from "0-shared/components/WorkSpace/WorkSpace";
 import { Note } from "1-entities/components/Note/Note";
 import { TreeViewer } from "3-widgets/components/TreeViewer/TreeViewer";
+import { ToolBar } from "3-widgets/components/ToolBar/ToolBar";
 
 type TBasePageprops = {
     addClassNames?: string[];
@@ -16,10 +17,11 @@ function BasePage({ addClassNames = [] }: TBasePageprops) {
     return (
         <div className={genClassName}>
             <HeaderBar></HeaderBar>
+            <ToolBar addClassNames={["BasePage__toolbar"]}></ToolBar>
             <div className="BasePage__workSpace_wrapper">
                 <TreeViewer></TreeViewer>
                 <WorkSpace addClassNames={["BasePage__workSpace"]}>
-                    <Note headerText="hello world"></Note>
+                    <Note></Note>
                 </WorkSpace>
             </div>
         </div>
