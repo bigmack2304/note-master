2cdaccc829bfbf62f6b5699b3ef88baf3be25cf0
bigmack2304 <dumon7@yandex.ru>
2024-02-03 22:54:45 +0700

    теперь note может отрисоввывать выбранную заметку

--- src/1-entities/components/Note/Note.tsx
+++ src/1-entities/components/Note/Note.tsx
@@ -5,6 +5,7 @@ import type { PaletteMode } from "@mui/material";
 import { useTemeMode } from "0-shared/hooks/useThemeMode";
 import "./Note.scss";
 import { EditableHeader } from "2-features/components/EditableHeader/EditableHeader";
+import { useAppSelector } from "0-shared/hooks/useAppSelector";
 
 type TNoteProps = {
     headerText?: string;
@@ -22,9 +23,17 @@ function Note({ headerText, addClassNames = [] }: TNoteProps) {
     const defaultClassName = "note";
     const genClassName = defaultClassName.split(" ").concat(addClassNames).join(" ");
     const themeValue = useTemeMode();
+    const currentNote = useAppSelector((store) => store.saveDataInspect.currentNote);
+
     return (
         <Box className={genClassName} component={"div"} sx={noteStyles(themeValue)}>
-            <EditableHeader defaultText={headerText} />
+            {currentNote && currentNote.body.length > 0
+                ? currentNote.body.map((note) => {
+                      if (note.component === "header") {
+                          return <EditableHeader defaultText={note.value} key={note.id} edit_id={note.id} />;
+                      }
+                  })
+                : null}
         </Box>
     );
 }
