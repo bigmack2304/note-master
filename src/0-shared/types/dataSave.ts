3ab97276c9afba901c8d0aca77e1e48c4342b5c2
bigmack2304 <dumon7@yandex.ru>
2024-02-04 22:35:20 +0700

    правки типов

--- src/0-shared/types/dataSave.ts
+++ src/0-shared/types/dataSave.ts
@@ -1,63 +1,68 @@
 // варианты своиств в зависимости от компонента внутри заметки
 // заголовок
 interface TBodyComponentHeader {
-    component: "header";
-    value: string;
+  component: "header";
+  value: string;
 }
 
 // текст
 interface TBodyComponentText {
-    component: "text";
-    texValue: string;
+  component: "text";
+  texValue: string;
 }
 //////////////////////////////////////////////////////////////////
 
-type nodeType = "component" | "note" | "folder";
+type TNodeType = "component" | "note" | "folder";
 
 // общие поля компонентов внутри заметки
-interface IDataTreeitemBody {
-    id: string; // глобальный id во всем дереве
-    type: nodeType;
+interface IDataTreeNode {
+  id: string; // глобальный id во всем дереве
+  type: TNodeType;
 }
 
 // тип поля body в заметке
 // TODO: потом тип пойдет по такому примеру
 //type TNoteBody = IDataTreeitemBody & (TBodyComponentHeader | TBodyComponentText);
-type TNoteBody = IDataTreeitemBody & TBodyComponentHeader;
+type TNoteBody = IDataTreeNode & TBodyComponentHeader;
 
 // типизация тега заметок
 interface IGlobalTag {
-    tag_name: string;
-    color: string;
-    text: string;
+  tag_name: string;
+  color: string;
+  text: string;
 }
 
 // тип, папка или заметка
 type TchildrenType = IDataTreeFolder | IDataTreeNote;
 
 // типизация заметки
-interface IDataTreeNote {
-    id: string; // глобальный id во всем дереве
-    tags?: IGlobalTag["tag_name"][];
-    name: string;
-    type: nodeType;
-    body: TNoteBody[];
+interface IDataTreeNote extends IDataTreeNode {
+  tags?: IGlobalTag["tag_name"][];
+  name: string;
+  body: TNoteBody[];
 }
 
 // типизация папки
-interface IDataTreeFolder {
-    name: string;
-    color: string;
-    id: string; // глобальный id во всем дереве
-    type: nodeType;
-    children?: TchildrenType[];
+interface IDataTreeFolder extends IDataTreeNode {
+  name: string;
+  color: string;
+  children?: TchildrenType[];
 }
 
 // типизация фаила с сохранением
 interface IDataSave {
-    db_type: string;
-    global_tags: IGlobalTag[];
-    data_tree: IDataTreeFolder;
+  db_type: string;
+  global_tags: IGlobalTag[];
+  data_tree: IDataTreeFolder;
 }
 
-export type { IDataSave, IDataTreeFolder, IDataTreeNote, IGlobalTag, TNoteBody, TchildrenType, nodeType, IDataTreeitemBody };
+export type {
+  IDataSave,
+  IDataTreeFolder,
+  IDataTreeNote,
+  IGlobalTag,
+  TNoteBody,
+  TchildrenType,
+  TNodeType,
+  IDataTreeNode,
+};
--- src/0-shared/utils/typeHelpers.ts
+++ src/0-shared/utils/typeHelpers.ts
@@ -1,5 +1,10 @@
 import { ForwardRefRenderFunction } from "react";
-import type { IDataTreeNote, IDataTreeFolder, TNoteBody } from "0-shared/types/dataSave";
+import type {
+  IDataTreeNote,
+  IDataTreeFolder,
+  TNoteBody,
+  TchildrenType,
+} from "0-shared/types/dataSave";
 
 // вычисляет пропсы компонента
 type GetProps<T> = T extends React.ComponentType<infer Props> ? Props : unknown;
@@ -9,18 +14,22 @@ type GetProps<T> = T extends React.ComponentType<infer Props> ? Props : unknown;
 type Ref<T> = Parameters<ForwardRefRenderFunction<T>>[1];
 
 // проверяет чтобы сущьность пренадлежала к типу IDataTreeNote
-function isDataTreeNote(val: any): val is IDataTreeNote {
-    return val.type === "note";
+function isDataTreeNote(
+  node: TchildrenType | TNoteBody
+): node is IDataTreeNote {
+  return node.type === "note";
 }
 
 // проверяет чтобы сущьность пренадлежала к типу IDataTreeFolder
-function isDataTreeFolder(val: any): val is IDataTreeFolder {
-    return val.type === "folder";
+function isDataTreeFolder(
+  node: TchildrenType | TNoteBody
+): node is IDataTreeFolder {
+  return node.type === "folder";
 }
 
 // проверяет чтобы сущьность пренадлежала к типу TNoteBody
-function isDataNoteBody(val: any): val is TNoteBody {
-    return val.type === "component";
+function isDataNoteBody(node: TchildrenType | TNoteBody): node is TNoteBody {
+  return node.type === "component";
 }
 
 export { isDataTreeNote, isDataTreeFolder, isDataNoteBody };
