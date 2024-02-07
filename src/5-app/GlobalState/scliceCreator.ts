b946cf9fc89dcdef679f714bc68505416bf3b220
bigmack2304 <dumon7@yandex.ru>
2024-02-08 02:17:43 +0700

    createAppSlice перенес в отдельный фаил

--- src/5-app/GlobalState/scliceCreator.ts
+++ src/5-app/GlobalState/scliceCreator.ts
@@ -1,5 +1,7 @@
 import { buildCreateSlice, asyncThunkCreator } from "@reduxjs/toolkit";
-
+/**
+ * надстройка для createSlice, позволяет создавать асинхронные редьюсеры
+ */
 const createAppSlice = buildCreateSlice({
     creators: { asyncThunk: asyncThunkCreator },
 });
