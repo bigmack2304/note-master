62fd3377ee75e2d28979dc2cf3f68b011a0b3ea4
bigmack2304 <dumon7@yandex.ru>
2024-02-10 00:35:54 +0700

    незаконченный хук получения списка тегов

---
+++ src/0-shared/hooks/useTags.ts
@@ -0,0 +1,11 @@
+import React from "react";
+import type { IAllTags } from "0-shared/types/dataSave";
+
+/**
+ * возвращает global_tags из indexed db
+ */
+// function useTags(): IAllTags {
+//     return;
+// }
+
+// export { useTags };
