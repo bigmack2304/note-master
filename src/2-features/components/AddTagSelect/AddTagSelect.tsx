5a315e5a473c0fd226063c81ed1c60d4e376d9ab
bigmack2304 <dumon7@yandex.ru>
2024-02-11 18:47:23 +0700

    визуальные правки
    + добавил недостающий key

--- src/2-features/components/AddTagSelect/AddTagSelect.tsx
+++ src/2-features/components/AddTagSelect/AddTagSelect.tsx
@@ -12,16 +12,19 @@ import { useTags } from "0-shared/hooks/useTags";
 import { NoteTag } from "0-shared/components/NoteTag/NoteTag";
 import type { IGlobalTag } from "0-shared/types/dataSave";
 import type { PaletteMode, SxProps } from "@mui/material";
+import { useTemeMode } from "0-shared/hooks/useThemeMode";
 
 type TAddTagSelectProps = {
     onChange?: (tagNames: string | string[]) => void;
 };
 
-const addTagSelectStyle = (tag: IGlobalTag, isColored: boolean) => {
+const addTagSelectStyle = (tag: IGlobalTag, isColored: boolean, theme: PaletteMode) => {
     let style = {} as any;
 
     if (isColored) {
-        style.backgroundColor = `color-mix(in hsl, ${tag.color}, #FFFFFF00 50%)`;
+        let colorMix = theme === "light" ? "#FFFFFF" : "#000000";
+        let colorOpacyty = theme === "light" ? "30%" : "85%";
+        style.backgroundColor = `color-mix(in srgb-linear, ${tag.color}, ${colorMix} ${colorOpacyty})`;
     }
 
     return style as SxProps;
@@ -36,6 +39,7 @@ function AddTagSelect({ onChange }: TAddTagSelectProps) {
     const currentNote = useAppSelector((state) => state.saveDataInspect.currentNote);
     const tagColored = useAppSelector((state) => state.settingsData.highlightingTagsInForms);
     const allTags = useTags();
+    const themeValue = useTemeMode();
 
     // массив доступных тегов. (если в активной заметке есть какието теги то они считаются недоступными)
     const availableTags = (function () {
@@ -68,14 +72,14 @@ function AddTagSelect({ onChange }: TAddTagSelectProps) {
                 renderValue={(selected) => (
                     <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
                         {selected.map((tagName) => {
-                            return <NoteTag isEdit={false} tagObj={allTags![tagName]} />;
+                            return <NoteTag isEdit={false} tagObj={allTags![tagName]} key={tagName} />;
                         })}
                     </Box>
                 )}
             >
                 {availableTags.map((tag) => {
                     return (
-                        <MenuItem value={tag.tag_name} key={tag.tag_name} sx={addTagSelectStyle(tag, tagColored)}>
+                        <MenuItem value={tag.tag_name} key={tag.tag_name} sx={addTagSelectStyle(tag, tagColored, themeValue)}>
                             {tag.tag_name}
                         </MenuItem>
                     );
