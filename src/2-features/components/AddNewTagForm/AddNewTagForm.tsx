0a994f5a41a27c829fd462504c3c7915549f1405
bigmack2304 <dumon7@yandex.ru>
2024-02-12 20:43:01 +0700

    компонент для добавления нового тега

---
+++ src/2-features/components/AddNewTagForm/AddNewTagForm.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Typography, TextField } from "@mui/material";
+import { ColorTagSelect } from "../ColorTagSelect/ColorTagSelect";
+import { OkButton } from "0-shared/components/OkButton/OkButton";
+import type { TTagColors } from "0-shared/types/dataSave";
+import { projectAddNewTag } from "5-app/GlobalState/saveDataInspectStore";
+import { useAppDispatch } from "0-shared/hooks/useAppDispatch";
+import "./AddNewTagForm.scss";
+
+type TAddNewTagFormProps = {
+    addClassNames?: string[];
+};
+
+/**
+ * список всех тегов с возможностью их
+ * @prop addClassNames - массив строк, которые будут применены к компоненту в качестве доп.классов
+ */
+function AddNewTagForm({ addClassNames = [] }: TAddNewTagFormProps) {
+    const defaultClassName = "AddNewTagForm";
+    const genClassName = defaultClassName.split(" ").concat(addClassNames).join(" ");
+    const [newTagName, setNewTagName] = useState<string>("");
+    const [selectValue, setSelectValue] = useState<TTagColors | "">("");
+    const dispatch = useAppDispatch();
+
+    const onFormSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        dispatch(projectAddNewTag({ tagColor: selectValue as TTagColors, tagName: newTagName }));
+
+        setNewTagName("");
+    };
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewTagName(e.target.value);
+    };
+
+    const onSelectChange = (selectColor: TTagColors | "") => {
+        setSelectValue(selectColor);
+    };
+
+    return (
+        <form autoComplete="off" className={genClassName} onSubmit={onFormSubmit}>
+            <fieldset className="AddNewTagForm__fieldset">
+                <legend>
+                    <Typography variant="caption">Добавить новый тег</Typography>
+                </legend>
+                <div className="AddNewTagForm__inner">
+                    <div className="AddNewTagForm__inputs">
+                        <TextField value={newTagName} label="Новый тег" placeholder="Имя тега" variant="standard" onChange={onInputChange} required />
+                        <ColorTagSelect onChange={onSelectChange} required />
+                    </div>
+                    <OkButton type="submit" />
+                </div>
+            </fieldset>
+        </form>
+    );
+}
+
+export { AddNewTagForm };
