a1b3df1389436e305affb3bb1609b13f7dbb2025
bigmack2304 <dumon7@yandex.ru>
2024-02-23 15:50:39 +0700

    правки useEventListener
    - можно устанавливать обьект для прослушивания
    - в колбек возвращается обьект события
    - поправлены типы

--- src/0-shared/hooks/useEventListener.ts
+++ src/0-shared/hooks/useEventListener.ts
@@ -1,37 +1,41 @@
 import React, { useEffect, useCallback } from "react";
 import { useHandleUpdate } from "./useHandleUpdate";
 
-type TuseEventListenerProps = {
+type TuseEventListenerProps<E extends Event> = {
     updateOnReset?: boolean;
     eventName?: string | string[];
-    onEvent?: () => void;
+    eventTarget?: Window | Document | HTMLElement;
+    onEvent?: (e: E) => void;
 };
 
 /**
  * позволяет подписатся на любое событие и выполнить какието действия при его появлении
+ * @ не требует мемоизирования входящих параметров, не требоует следнить за отпиской
+ *
  * @prop updateOnReset - (default: false) нужноли выполнить перерендер компонента
  * @prop onEvent - (default: undefuned) колбек который отработает после появления события
  * @prop eventName - (default: undefuned) имя события на которое подписываемся
+ * @prop eventTarget - (default: window) обьект на котором прослушиваем событие,
  */
-function useEventListener({ updateOnReset, onEvent, eventName }: TuseEventListenerProps) {
+function useEventListener<E extends Event>({ updateOnReset, onEvent, eventName, eventTarget = window }: TuseEventListenerProps<E>) {
     const [handleupdate] = useHandleUpdate();
 
-    const onEvt = useCallback(() => {
-        onEvent && onEvent();
+    const onEvt = useCallback((e: E) => {
+        onEvent && onEvent(e);
 
         if (updateOnReset) {
             handleupdate();
         }
-    }, []);
+    }, []) as EventListener;
 
     useEffect(() => {
         if (eventName) {
             if (Array.isArray(eventName)) {
                 for (let nameItem of eventName) {
-                    window.addEventListener(nameItem, onEvt);
+                    eventTarget.addEventListener(nameItem, onEvt);
                 }
             } else {
-                window.addEventListener(eventName, onEvt);
+                eventTarget.addEventListener(eventName, onEvt);
             }
         }
 
@@ -39,10 +43,10 @@ function useEventListener({ updateOnReset, onEvent, eventName }: TuseEventListen
             if (eventName) {
                 if (Array.isArray(eventName)) {
                     for (let nameItem of eventName) {
-                        window.removeEventListener(nameItem, onEvt);
+                        eventTarget.removeEventListener(nameItem, onEvt);
                     }
                 } else {
-                    window.removeEventListener(eventName, onEvt);
+                    eventTarget.removeEventListener(eventName, onEvt);
                 }
             }
         };
