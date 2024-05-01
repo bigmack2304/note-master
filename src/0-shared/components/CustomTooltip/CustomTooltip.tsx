e9dbf31b7a145261141ae1fdcf4b579bb7da57e1
bigmack2304 <dumon7@yandex.ru>
2024-05-01 15:22:31 +0700

    добавил tooltip
    всплывающее описание

---
+++ src/0-shared/components/CustomTooltip/CustomTooltip.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Tooltip from "@mui/material/Tooltip";
+import { useTemeMode } from "0-shared/hooks/useThemeMode";
+import "./CustomTooltip.scss";
+import * as style from "./CustomTooltipStyle";
+import type { TooltipProps } from "@mui/material/Tooltip";
+
+/**
+ * Всплывающее описание
+ */
+function CustomTooltip({ className, ...props }: TooltipProps) {
+    const themeValue = useTemeMode();
+    const componentClassName = "CustomToolTip";
+    return (
+        <Tooltip
+            {...props}
+            arrow
+            className={componentClassName}
+            classes={{ popper: `${className ? className : ""} ${componentClassName}` }}
+            componentsProps={{ popper: { sx: style.customToolTipStyle(themeValue) } }}
+            enterDelay={400}
+        />
+    );
+}
+
+export { CustomTooltip };
